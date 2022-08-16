7d90f91f5b043b7180d72a3578688b88e0c5735d
NhuMo Dang <nhumo@careloop.io>
2022-08-16 16:41:50 +0200

    eyeroll

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Defines the root path route ('/')
+  # root 'tasks#index'
+
+  # Read
+  get 'tasks', to: 'tasks#index'
+  # Create
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
